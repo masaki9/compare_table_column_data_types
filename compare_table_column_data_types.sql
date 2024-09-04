4de8cd4f478a3a6f4d29e66de765236c0fdee8de
masaki <40445257+masaki9@users.noreply.github.com>
2024-09-04 12:44:31 -0700

    Update formatting in compare_table_column_data_types.sql

--- compare_table_column_data_types.sql
+++ compare_table_column_data_types.sql
@@ -1,23 +1,34 @@
 select
-	src.column_name as source_column, src.data_type as source_type,
-    dst.column_name as target_column, dst.data_type as target_type,
+    src.column_name as source_column,
+    src.data_type as source_type,
+    dst.column_name as target_column,
+    dst.data_type as target_type,
     case
-		when dst.data_type = src.data_type then true
+        when dst.data_type = src.data_type then true
         else false
     end as is_data_type_same
 from
-    (select ordinal_position, column_name, data_type
-    from information_schema.columns
-    where
-		table_schema = 'test' and
-        table_name = 'source_tbl'
+    (
+        select
+            ordinal_position,
+            column_name,
+            data_type
+        from
+            information_schema.columns
+        where
+            table_schema = 'test'
+            and table_name = 'source_tbl'
     ) src
-join
-    (select ordinal_position, column_name, data_type
-    from information_schema.columns
-    where
-        table_schema = 'test' and
-        table_name = 'target_tbl'
-    ) dst
-on src.column_name = dst.column_name
-order by src.ordinal_position asc;
+    join (
+        select
+            ordinal_position,
+            column_name,
+            data_type
+        from
+            information_schema.columns
+        where
+            table_schema = 'test'
+            and table_name = 'target_tbl'
+    ) dst on src.column_name = dst.column_name
+order by
+    src.ordinal_position asc;
